8443bd7d6fd9ed848b31abd0b63e0392beac78d1
Ahsanulnas <65066333+ahsnl-py@users.noreply.github.com>
2020-06-29 15:11:31 +0200

    Update settings.py

--- esicProject/settings.py
+++ esicProject/settings.py
@@ -130,7 +130,7 @@ LOGIN_REDIRECT_URL = 'blog:post_list'
 LOGIN_URL = 'login'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ahsanul.nas@gmail.com'
-EMAIL_HOST_PASSWORD = 'RFFw4nSPvPrL9faN'
+EMAIL_HOST_USER = 'admin'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
