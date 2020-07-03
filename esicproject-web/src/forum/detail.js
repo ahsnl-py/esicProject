6308b1b81ef911ed9517c4c178fb7e7115fac531
ahsnl <ahsanul.nas@gmail.com>
2020-07-03 09:10:42 +0200

    Linking Individual Forum

--- esicproject-web/src/forum/detail.js
+++ esicproject-web/src/forum/detail.js
@@ -14,35 +14,46 @@ export function ParentForum(props){
   }
   
   // make some change here .. continue from series 70
-  export function Forum(props) {
-      const {forum, didRepost} = props
-      const [actionForum, setActionForum, hideAction] = useState(props.forum ? props.forum : null)
-      const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
-      
-      const handlePerformAction = (newActionForum, status ) => {
-        if (status === 2000) {
-          setActionForum(newActionForum)
-        } else if (status === 201 ) {
-          if (didRepost) {
-            didRepost(newActionForum)
-          }
-          //let the list know
+export function Forum(props) {
+    const {forum, didRepost} = props
+    const [actionForum, setActionForum, hideAction] = useState(props.forum ? props.forum : null)
+    const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
+    const path = window.location.pathname
+    const match = path.match(/(?<forumid>\d+)/)
+    const urlForumId = match ? match.groups.forumid : -1
+
+    const isDetail = `${forum.id}` === `${urlForumId}` 
+    const handleLink = (event) => {
+      event.preventDefault()
+      window.location.href = `/${forum.id}`
+    }
+    const handlePerformAction = (newActionForum, status ) => {
+      if (status === 200) {
+        setActionForum(newActionForum)
+      } else if (status === 201 ) {
+        if (didRepost) {
+          didRepost(newActionForum)
         }
-  
-        //set action forum
+        //let the list know
       }
-  
-  //continue here...
-      return <div className={className}>
-              <div>
-                <p>{forum.id} - {forum.content}</p>
-                <ParentForum forum={forum} />
-              </div>
-          {(actionForum && hideAction !== true ) && <div className='btn btn-group'>
-            <ActionBtn forum={actionForum} didPerformAction={handlePerformAction} action={{type: "like", display:"Likes"}}/>
-            <ActionBtn forum={actionForum} didPerformAction={handlePerformAction} action={{type: "unlike", display:"Unlike"}}/>
-            <ActionBtn forum={actionForum} didPerformAction={handlePerformAction} action={{type: "repost", display:"Repost"}}/>
-          </div>
+
+      //set action forum
+    }
+
+    //continue here...
+    return <div className={className}>
+            <div>
+              <p>{forum.id} - {forum.content}</p>
+              <ParentForum forum={forum} />
+            </div>
+            <div className='btn btn-group'>
+        {(actionForum && hideAction !== true ) && <React.Fragment>
+          <ActionBtn forum={actionForum} didPerformAction={handlePerformAction} action={{type: "like", display:"Likes"}}/>
+          <ActionBtn forum={actionForum} didPerformAction={handlePerformAction} action={{type: "unlike", display:"Unlike"}}/>
+          <ActionBtn forum={actionForum} didPerformAction={handlePerformAction} action={{type: "repost", display:"Repost"}}/>
+          </React.Fragment>
+}  
+          {isDetail === true ? null : <button className="btn btn-outline-primary btn-sm" onClick={handleLink}>View</button>}
+        </div>
+    </div>
   }
-      </div>
-      }
